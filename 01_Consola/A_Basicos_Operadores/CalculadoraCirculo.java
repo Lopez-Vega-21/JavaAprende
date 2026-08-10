397fedeaa60bb533b6953bbfd7cdec0db987e90d
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-10 17:26:47 -0600

    Actualizar CalculadoraCirculo.java

--- 01_Consola/A_Basicos_Operadores/CalculadoraCirculo.java
+++ 01_Consola/A_Basicos_Operadores/CalculadoraCirculo.java
@@ -1,16 +1,20 @@
-/**
- * 
- * Reto 2: Perímetro y área de un círculo
- * Descripción: Solicitar el radio de un círculo y entregar el perímetro y el área.
- */
 import java.util.Scanner;
 
-public class CalculadoraCirculo {
+public class CalculadoraEdad {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        // TODO: Solicitar el radio
-        // TODO: Calcular el perímetro (2 * PI * radio)
-        // TODO: Calcular el área (PI * radio^2)
-        // TODO: Mostrar los resultados
+
+        // 1. Solicitar la edad
+        System.out.print("Ingresa tu edad en años: ");
+        int edad = teclado.nextInt();
+
+        // 2. Cálculos y 3. Mostrar los resultados
+        System.out.println("Décadas: " + (edad / 10));
+        System.out.println("Lustros: " + (edad / 5));
+        System.out.println("Meses: " + (edad * 12));
+        System.out.println("Semanas: " + (edad * 52));
+        System.out.println("Días: " + (edad * 365));
+        System.out.println("Horas: " + (edad * 8760));
+        System.out.println("Minutos: " + (edad * 525600));
     }
 }
