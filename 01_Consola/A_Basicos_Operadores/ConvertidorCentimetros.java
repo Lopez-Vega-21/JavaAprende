d871a9823290b8ccc76133273b54b32f5bd5ee29
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-10 18:02:24 -0600

    Actualizar ConvertidorCentimetros.java

--- 01_Consola/A_Basicos_Operadores/ConvertidorCentimetros.java
+++ 01_Consola/A_Basicos_Operadores/ConvertidorCentimetros.java
@@ -1,14 +1,19 @@
-/**
- * Reto 4: Centímetros a Pulgadas
- * Descripción: Convierta de centímetros a pulgadas (1 pulgada = 2.54 cm).
- */
 import java.util.Scanner;
 
 public class ConvertidorCentimetros {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        // TODO: Solicitar los centímetros
-        // TODO: Realizar la conversión dividiendo entre 2.54
-        // TODO: Imprimir el resultado en el formato solicitado
+        
+        // 1. Solicitar los centímetros
+        System.out.print("Ingresa la cantidad en centímetros: ");
+        double centimetros = teclado.nextDouble();
+        
+        // 2. Realizar la conversión dividiendo entre 2.54
+        double pulgadas = centimetros / 2.54;
+        
+        // 3. Imprimir el resultado en el formato solicitado
+        System.out.println(centimetros + " cm equivalen a " + pulgadas + " pulgadas.");
+        
+        teclado.close();
     }
 }
