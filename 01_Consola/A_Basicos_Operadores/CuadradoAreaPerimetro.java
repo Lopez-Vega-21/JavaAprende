0636bb5b03a59b956eb508c3c3cc2bc1c8c01790
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-10 18:04:55 -0600

    Actualizar CuadradoAreaPerimetro.java

--- 01_Consola/A_Basicos_Operadores/CuadradoAreaPerimetro.java
+++ 01_Consola/A_Basicos_Operadores/CuadradoAreaPerimetro.java
@@ -1,14 +1,21 @@
-/**
- * Reto 8: Área y perímetro de un cuadrado
- * Descripción: Solicitar el valor de un lado del cuadrado, y calcular área y perímetro.
- */
 import java.util.Scanner;
 
 public class CuadradoAreaPerimetro {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        // TODO: Solicitar el lado
-        // TODO: Calcular área (lado * lado) y perímetro (lado * 4)
-        // TODO: Imprimir resultados
+        
+        // 1. Solicitar el lado
+        System.out.print("Ingresa el valor del lado del cuadrado: ");
+        double lado = teclado.nextDouble();
+        
+        // 2. Calcular área (lado * lado) y perímetro (lado * 4)
+        double area = lado * lado;
+        double perimetro = lado * 4;
+        
+        // 3. Imprimir resultados
+        System.out.println("El área del cuadrado es: " + area);
+        System.out.println("El perímetro del cuadrado es: " + perimetro);
+        
+        teclado.close();
     }
 }
