50c0851a7609fe8ca86351037217d80bf06dbb46
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 17:20:24 -0600

    Actualizar GalonesALitros.java

--- 01_Consola/A_Basicos_Operadores/GalonesALitros.java
+++ 01_Consola/A_Basicos_Operadores/GalonesALitros.java
@@ -1,14 +1,19 @@
-/**
- * Reto 17: Galones a Litros
- * Descripción: Convertir G galones a L litros (1 galón = 3.78541 litros).
- */
 import java.util.Scanner;
 
 public class GalonesALitros {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        // TODO: Solicitar galones
-        // TODO: Convertir a litros
-        // TODO: Imprimir
+        
+        // 1. Solicitar galones
+        System.out.print("Ingresa la cantidad en galones: ");
+        double galones = teclado.nextDouble();
+        
+        // 2. Convertir a litros
+        double litros = galones * 3.78541;
+        
+        // 3. Imprimir
+        System.out.println(galones + " galones equivalen a " + litros + " litros.");
+        
+        teclado.close();
     }
 }
