5eb29674476573c3be724299d650c3f5698e5677
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 19:03:13 -0600

    Actualizar ConvertidorCentimetros.java

--- 01_Consola/A_Basicos_Operadores/ConvertidorCentimetros.java
+++ 01_Consola/A_Basicos_Operadores/ConvertidorCentimetros.java
@@ -4,14 +4,11 @@ public class ConvertidorCentimetros {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        // 1. Solicitar los centímetros
         System.out.print("Ingresa la cantidad en centímetros: ");
         double centimetros = teclado.nextDouble();
         
-        // 2. Realizar la conversión dividiendo entre 2.54
         double pulgadas = centimetros / 2.54;
         
-        // 3. Imprimir el resultado en el formato solicitado
         System.out.println(centimetros + " cm equivalen a " + pulgadas + " pulgadas.");
         
         teclado.close();
