e453576b11ad705961874044be7ff2de5d2b0704
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 18:30:55 -0600

    Actualizar PiesAMetros.java

--- 01_Consola/A_Basicos_Operadores/PiesAMetros.java
+++ 01_Consola/A_Basicos_Operadores/PiesAMetros.java
@@ -1,14 +1,19 @@
-/**
- * Reto 14: Pies a Metros
- * Descripción: Convertir X pies a M metros. (1 pie = 0.3048 m).
- */
 import java.util.Scanner;
 
 public class PiesAMetros {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        // TODO: Solicitar medida en pies
-        // TODO: Convertir a metros
-        // TODO: Imprimir
+        
+        // 1. Solicitar medida en pies
+        System.out.print("Ingresa la medida en pies: ");
+        double pies = teclado.nextDouble();
+        
+        // 2. Convertir a metros
+        double metros = pies * 0.3048;
+        
+        // 3. Imprimir
+        System.out.println(pies + " pies equivalen a " + metros + " metros.");
+        
+        teclado.close();
     }
 }
