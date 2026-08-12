7c5b3682eee7266e32b4a51a7fc866f61238feac
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 19:01:27 -0600

    Actualizar PiesAMetros.java

--- 01_Consola/A_Basicos_Operadores/PiesAMetros.java
+++ 01_Consola/A_Basicos_Operadores/PiesAMetros.java
@@ -4,14 +4,11 @@ public class PiesAMetros {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        // 1. Solicitar medida en pies
         System.out.print("Ingresa la medida en pies: ");
         double pies = teclado.nextDouble();
         
-        // 2. Convertir a metros
         double metros = pies * 0.3048;
         
-        // 3. Imprimir
         System.out.println(pies + " pies equivalen a " + metros + " metros.");
         
         teclado.close();
