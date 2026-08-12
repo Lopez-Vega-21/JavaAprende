5eb41dc9646f960c63c293518be52c1bf4c68784
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 18:49:22 -0600

    Actualizar AreaTriangulo.java

--- 01_Consola/A_Basicos_Operadores/AreaTriangulo.java
+++ 01_Consola/A_Basicos_Operadores/AreaTriangulo.java
@@ -4,17 +4,14 @@ public class AreaTriangulo {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        // 1. Solicitar base y altura
         System.out.print("Ingresa la base del triángulo: ");
         double base = teclado.nextDouble();
 
         System.out.print("Ingresa la altura del triángulo: ");
         double altura = teclado.nextDouble();
 
-        // 2. Calcular el área
         double area = (base * altura) / 2;
 
-        // 3. Imprimir resultado
         System.out.println("El área del triángulo es: " + area);
     }
-}<
+}
