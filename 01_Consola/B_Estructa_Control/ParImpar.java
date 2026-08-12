bcd0135b43871969ab1be8e397bece1f89049359
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 19:16:16 -0600

    Actualizar ParImpar.java

--- 01_Consola/B_Estructa_Control/ParImpar.java
+++ 01_Consola/B_Estructa_Control/ParImpar.java
@@ -1,16 +1,16 @@
-/**
- * Reto 11: Análisis de un número (Par, Impar y Primo)
- * Descripción: Solicitar un número al usuario y determinar si es par o impar, 
- */
 import java.util.Scanner;
 
 public class ParImpar {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        // TODO: Solicitar un número entero al usuario
+        System.out.print("Ingrese un número entero: ");
+        int numero = teclado.nextInt();
         
-        // 1. EVALUAR PAR O IMPAR
-        // TODO: Usar if/else  para imprimir si es par o impar.
+        if (numero % 2 == 0) {
+            System.out.println("El número es par.");
+        } else {
+            System.out.println("El número es impar.");
+        }
     }
 }
