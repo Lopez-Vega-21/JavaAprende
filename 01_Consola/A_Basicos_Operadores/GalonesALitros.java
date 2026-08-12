dbcf5447b6e675616ab5c8072cee5b8a49467927
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 19:00:20 -0600

    Actualizar GalonesALitros.java

--- 01_Consola/A_Basicos_Operadores/GalonesALitros.java
+++ 01_Consola/A_Basicos_Operadores/GalonesALitros.java
@@ -4,14 +4,11 @@ public class GalonesALitros {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        // 1. Solicitar galones
         System.out.print("Ingresa la cantidad en galones: ");
         double galones = teclado.nextDouble();
         
-        // 2. Convertir a litros
         double litros = galones * 3.78541;
         
-        // 3. Imprimir
         System.out.println(galones + " galones equivalen a " + litros + " litros.");
         
         teclado.close();
