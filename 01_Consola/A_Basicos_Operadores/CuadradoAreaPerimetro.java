6ba3326310bf29575de66718590b5171932e60e6
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 19:02:19 -0600

    Actualizar CuadradoAreaPerimetro.java

--- 01_Consola/A_Basicos_Operadores/CuadradoAreaPerimetro.java
+++ 01_Consola/A_Basicos_Operadores/CuadradoAreaPerimetro.java
@@ -4,15 +4,12 @@ public class CuadradoAreaPerimetro {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        // 1. Solicitar el lado
         System.out.print("Ingresa el valor del lado del cuadrado: ");
         double lado = teclado.nextDouble();
         
-        // 2. Calcular área (lado * lado) y perímetro (lado * 4)
         double area = lado * lado;
         double perimetro = lado * 4;
         
-        // 3. Imprimir resultados
         System.out.println("El área del cuadrado es: " + area);
         System.out.println("El perímetro del cuadrado es: " + perimetro);
         
