7275d8bc7852c767c6979c8e69f9674f7fc91acc
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 19:17:41 -0600

    Actualizar SalarioDinamico.java

--- 01_Consola/B_Estructa_Control/SalarioDinamico.java
+++ 01_Consola/B_Estructa_Control/SalarioDinamico.java
@@ -1,10 +1,22 @@
-/**
- * Reto 2: Salario Obrero (Repaso de expresiones lógicas)
- * Descripción: Recalcular el salario neto con 2% compensación y descuentos IMSS/ISPT.
- */
+import java.util.Scanner;
+
 public class SalarioDinamico {
     public static void main(String[] args) {
-        // TODO: Replicar lógica del Ejercicio 13 básico pero añadiendo validaciones
-        // TODO: (Ej. Si el total es negativo, mostrar error)
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.print("Ingrese el salario base: ");
+        double salarioBase = teclado.nextDouble();
+        
+        if (salarioBase < 0) {
+            System.out.println("Error: El salario no puede ser negativo.");
+        } else {
+            double compensacion = salarioBase * 0.02;
+            double subtotal = salarioBase + compensacion;
+            double imss = subtotal * 0.05;
+            double ispt = subtotal * 0.03;
+            double salarioNeto = subtotal - (imss + ispt);
+            
+            System.out.println("Salario neto recalculado con compensación y descuentos: $" + salarioNeto);
+        }
     }
 }
