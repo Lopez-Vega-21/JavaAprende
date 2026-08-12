0ddf82ed8e42319c55deddbbb8b30b3b250c188c
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 18:56:31 -0600

    Actualizar CalculadoraCirculo.java

--- 01_Consola/A_Basicos_Operadores/CalculadoraCirculo.java
+++ 01_Consola/A_Basicos_Operadores/CalculadoraCirculo.java
@@ -4,17 +4,13 @@ public class CalculadoraCirculo {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        // Solicitar el radio
         System.out.print("Introduce el radio del círculo: ");
         double radio = teclado.nextDouble();
         
-        // Calcular el perímetro (2 * PI * radio)
         double perimetro = 2 * Math.PI * radio;
         
-        // Calcular el área (PI * radio^2)
         double area = Math.PI * Math.pow(radio, 2);
         
-        // Mostrar los resultados
         System.out.println("El perímetro es: " + perimetro);
         System.out.println("El área es: " + area);
         
