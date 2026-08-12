66c2d8d16576d4c2e5ce3f064d0dbdf7e99ca87d
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 19:02:44 -0600

    Actualizar KilogramosLibras.java

--- 01_Consola/A_Basicos_Operadores/KilogramosLibras.java
+++ 01_Consola/A_Basicos_Operadores/KilogramosLibras.java
@@ -4,14 +4,11 @@ public class KilogramosLibras {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        // 1. Solicitar los kilogramos
         System.out.print("Ingresa el peso en kilogramos: ");
         double kilos = teclado.nextDouble();
         
-        // 2. Multiplicar por 2.2 para obtener las libras
         double libras = kilos * 2.2;
         
-        // 3. Mostrar el resultado
         System.out.println(kilos + " kg equivalen a " + libras + " libras.");
         
         teclado.close();
