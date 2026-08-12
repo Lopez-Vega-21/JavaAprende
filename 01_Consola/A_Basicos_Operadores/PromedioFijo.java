794cdf63ec3e2a99ac36ba20f36294b7336b7bf5
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 18:39:12 -0600

    Actualizar PromedioFijo.java

--- 01_Consola/A_Basicos_Operadores/PromedioFijo.java
+++ 01_Consola/A_Basicos_Operadores/PromedioFijo.java
@@ -1,11 +1,20 @@
-/**
- * Reto 10: Promedio de números estáticos
- * Descripción: Calcular el promedio de: 19, 17, 21, 09, 12, 07.
- */
+import java.util.Scanner;
+
 public class PromedioFijo {
     public static void main(String[] args) {
-        // TODO: Declarar variables para los 6 números o sumarlos directamente
-        // TODO: Calcular el promedio dividiendo entre 6.0
-        // TODO: Imprimir el resultado
+        // Declarar variables para los 6 números
+        double num1 = 19;
+        double num2 = 17;
+        double num3 = 21;
+        double num4 = 9;
+        double num5 = 12;
+        double num6 = 7;
+        
+        // Sumar y calcular el promedio dividiendo entre 6.0
+        double suma = num1 + num2 + num3 + num4 + num5 + num6;
+        double promedio = suma / 6.0;
+        
+        // Imprimir el resultado
+        System.out.println("El promedio es: " + promedio);
     }
 }
