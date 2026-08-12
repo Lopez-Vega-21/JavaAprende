59587bd9d8e1ea05eb8b3817255345f2ffd4fbef
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 19:13:24 -0600

    Actualizar MenorDeDos.java

--- 01_Consola/B_Estructa_Control/MenorDeDos.java
+++ 01_Consola/B_Estructa_Control/MenorDeDos.java
@@ -1,13 +1,21 @@
-/**
- * Reto 10: Menor de dos números
- * Descripción: Solicitar un par de números enteros y mostrar cuál es el menor.
- */
 import java.util.Scanner;
 
 public class MenorDeDos {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        // TODO: Solicitar 2 números
-        // TODO: Usar if/else para imprimir el menor
+        
+        System.out.print("Ingresa el primer número: ");
+        int num1 = teclado.nextInt();
+        
+        System.out.print("Ingresa el segundo número: ");
+        int num2 = teclado.nextInt();
+        
+        if (num1 < num2) {
+            System.out.println("El número menor es: " + num1);
+        } else if (num2 < num1) {
+            System.out.println("El número menor es: " + num2);
+        } else {
+            System.out.println("Ambos números son iguales.");
+        }
     }
 }
