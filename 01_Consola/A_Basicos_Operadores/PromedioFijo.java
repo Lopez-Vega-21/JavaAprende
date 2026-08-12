d5e7a304000ca0ca6cd21bee7140f79a2839cdc8
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 19:01:55 -0600

    Actualizar PromedioFijo.java

--- 01_Consola/A_Basicos_Operadores/PromedioFijo.java
+++ 01_Consola/A_Basicos_Operadores/PromedioFijo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class PromedioFijo {
     public static void main(String[] args) {
-        // Declarar variables para los 6 números
+        
         double num1 = 19;
         double num2 = 17;
         double num3 = 21;
@@ -10,11 +10,9 @@ public class PromedioFijo {
         double num5 = 12;
         double num6 = 7;
         
-        // Sumar y calcular el promedio dividiendo entre 6.0
         double suma = num1 + num2 + num3 + num4 + num5 + num6;
         double promedio = suma / 6.0;
         
-        // Imprimir el resultado
         System.out.println("El promedio es: " + promedio);
     }
 }
