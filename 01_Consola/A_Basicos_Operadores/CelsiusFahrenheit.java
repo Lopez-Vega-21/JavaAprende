bb81594f1f35357eb5bd60a52d0d8cbc1a644bea
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 18:59:52 -0600

    Actualizar CelsiusFahrenheit.java

--- 01_Consola/A_Basicos_Operadores/CelsiusFahrenheit.java
+++ 01_Consola/A_Basicos_Operadores/CelsiusFahrenheit.java
@@ -4,15 +4,11 @@ public class CelsiusFahrenheit {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        // 1. Solicitar grados Celsius
         System.out.print("Ingresa la temperatura en grados Celsius: ");
         double celsius = teclado.nextDouble();
 
-        // 2. Aplicar la fórmula: (C * 9/5) + 32 = F
-        // Usamos 9.0 y 5.0 para asegurar la precisión decimal
         double fahrenheit = (celsius * 9.0 / 5.0) + 32;
 
-        // 3. Imprimir resultado
         System.out.println(celsius + "°C equivalen a " + fahrenheit + "°F.");
     }
 }
