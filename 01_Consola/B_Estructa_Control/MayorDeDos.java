5d0189acc8f3a34b2924312be2d3522b4f19d9a5
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-11 19:14:36 -0600

    Actualizar MayorDeDos.java

--- 01_Consola/B_Estructa_Control/MayorDeDos.java
+++ 01_Consola/B_Estructa_Control/MayorDeDos.java
@@ -1,13 +1,21 @@
-/**
- * Reto 3: Mayor de dos números
- * Descripción: Solicite dos números enteros y muestre el mayor.
- */
 import java.util.Scanner;
 
 public class MayorDeDos {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        // TODO: Solicitar 2 números
-        // TODO: Usar un if/else para imprimir el mayor
+        
+        System.out.print("Ingrese el primer número: ");
+        int num1 = teclado.nextInt();
+        
+        System.out.print("Ingrese el segundo número: ");
+        int num2 = teclado.nextInt();
+        
+        if (num1 > num2) {
+            System.out.println("El número mayor es: " + num1);
+        } else if (num2 > num1) {
+            System.out.println("El número mayor es: " + num2);
+        } else {
+            System.out.println("Ambos números son iguales.");
+        }
     }
 }
