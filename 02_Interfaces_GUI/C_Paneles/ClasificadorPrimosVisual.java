4e16e4b2286d5be794831fe311ab958c6427243d
gomezvasconcelositzel-ui <gomezvasconcelositzel@gmail.com>
2026-08-13 00:35:13 -0600

    Actualizar ClasificadorPrimosVisual.java

--- 02_Interfaces_GUI/C_Paneles/ClasificadorPrimosVisual.java
+++ 02_Interfaces_GUI/C_Paneles/ClasificadorPrimosVisual.java
@@ -23,9 +23,18 @@ public class ClasificadorPrimosVisual extends JFrame {
         btnBuscar.addActionListener(e -> {
             areaResultados.setText("Primos encontrados:\n");
             
-            // TODO: Crear ciclo for del 2 al 100
-            // TODO: Validar si 'i' es primo
-            // TODO: Si es primo, agregarlo al área de texto con areaResultados.append(i + ", ");
+            for (int i = 2; i <= 100; i++) {
+    boolean esPrimo = true;
+    for (int j = 2; j <= i - 1; j++) {
+        if (i % j == 0) {
+            esPrimo = false;
+            break;
+        }
+    }
+    if (esPrimo) {
+        areaResultados.append(i + ", ");
+    }
+}
         });
         
         add(btnBuscar); add(new JScrollPane(areaResultados));
